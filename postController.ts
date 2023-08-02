cc64e95cb48a13f3dfd1808190286e4217754fa2
Michael-Ibraev <mikhail12.876@gmail.com>
2023-08-02 13:19:59 +0300

    added postController

--- postController.ts
+++ postController.ts
@@ -12,6 +12,61 @@ export class PostController{
         }
     }
 
-    //25:20
+    static async getAll(req: Request, res: Response){
+        try{
+            const posts = await Post.findAll();
+            return res.json(posts);
+        } catch (e) {
+            res.status(500).json(e);
+        }
+    }
 
+    static async getOne(req: Request, res: Response){
+        try{
+            const {id} = req.params;
+            if(!id){
+                res.status(400).json({message: "id не указан"});
+            }
+            const post = await Post.findByPk(id);
+            return res.json(post);
+        } catch (e) {
+            res.status(500).json(e);
+        }
+    }
+
+    static async update(req: Request, res: Response){
+        try{
+            const post = req.body;
+            if(!post.id){
+                res.status(400).json({message: "id не указан"});
+            }
+            const updatedPost = await Post.update(post, {
+                where: {
+                    id: post.id
+                }
+            })
+            console.log(updatedPost);
+            return res.json(await Post.findByPk(post.id));
+        } catch (e) {
+            res.status(500).json(e);
+        }
+    }
+
+    static async delete(req: Request, res: Response){
+        try{
+            const {id} = req.params;
+            if(!id){
+                res.status(400).json({message: "id не указан"});
+            }
+            const post = await Post.destroy({
+                where: {
+                    id: id
+                },
+                force: true
+            });
+            return res.json(post);
+        } catch (e) {
+            res.status(500).json(e);
+        }
+    }
 }
--- router.ts
+++ router.ts
@@ -5,9 +5,9 @@ import {PostController} from "./postController";
 export const router = Router();
 
 router.post("/posts", PostController.create);
-router.post("/get");
-router.post("/get/:id");
-router.post("/put");
-router.post("/delete/:id");
+router.get("/posts", PostController.getAll);
+router.get("/posts/:id", PostController.getOne);
+router.put("/posts", PostController.update);
+router.delete("/posts/:id", PostController.delete);
 
 
